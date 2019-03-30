98097af7e8d221239c2e4ab4dffdddb51c96f403
sony <sony@DESKTOP-8UF28TN>
2019-03-30 23:14:12 +0530

    winner was declared accordingly.

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -6,23 +6,45 @@ public class TicTacToe{
 public static int[][] ticbox;
 	public static int win(int [][]tic) {
 		//check all rows
-		//loop through rows from 0 to 3 and check if all the 3 places have same marks
- 		
+		//loop through rows from 0 to 3 and check if all the 3 places have same mark
 		
 		//check all cols
 		//loop through columns from 0 to 3 and check if all the 3 places have same marks
- 		
 		//check both diagonals 
 		
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				
+				if((tic[i][j]==1)&& ((i==0 && j<3)||(i==1 && j<3)||(i==2 && j<3)))
+				{
+					return 1;
+				}
+				else if(tic[j][i]==1)
+				{
+					return 1;
+				}
+				else if(i==j)
+				{
+					if(tic[i][j]==1)
+					{
+						return 1;
+					}
+					else 
+						return 2;
+					
+				}
+				else 
+					
+				  return 2;
+			}
+				
+			}
 		
 		
-		//write your code here !!!
-		
-		
-		
-		return 0;
+		return 0;		
 		
 	}
+	
 	
 	public static void printBox(int [][]tic) {
 		for(int i=0;i<3;i++) {
@@ -33,18 +55,24 @@ public static int[][] ticbox;
 		}
 	}
 	public static boolean validate(int a1,int a2,int [][] tic) {
+		
+		if((a1>=0 && a1<3)&&(a2>=0 && a2<3))
+				{
+			      if(tic[a1][a2]==0)
+			      {
+			    	  return true;
+			      }
+			      else
+			    	  return false;
+				}
+		else 
+			return false;
 		//check if a1 is between 0 & 3
 		//check if a2 is between 0 & 3
-		//check if the selected box is empty ie, already not marked by other player
+		//check if the selected box is empty i.e, already not marked by other player
 		//if all checks passed return true.
-		
-		
-		//Write your code here !!!
-		
-		
-		return false;
 	}
-	public static void main(String args[]) {
+	public static void main(String args[]) {  
 		
 		ticbox = new int[3][3];
 		for(int i=0;i<3;i++) {
@@ -84,7 +112,10 @@ public static int[][] ticbox;
 				boolean valid = validate(a1,a2,ticbox);
 				if(valid) {
 				ticbox[a1][a2] = 2;
+				if(ticbox[a1][a2]==2)
+				{
 				chk = win(ticbox);
+				}
 				turn = true;
 				limit --;
 				}
